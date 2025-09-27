4beb10f386172b8ec1d9ad6f5971547c9dad774c
emrebaykusak <emrebaykusak12@gmail.com>
2025-09-27 19:49:38 +0300

    Remove build artifacts from repo

--- src/utils/forecast.d.ts
+++
@@ -1,4 +0,0 @@
-type WeatherStackCallback = (error: string | undefined, data: string | undefined) => void;
-export declare const forecast: (lat: string, lon: string, callback: WeatherStackCallback) => void;
-export {};
-//# sourceMappingURL=forecast.d.ts.map
--- src/utils/geocode.d.ts
+++
@@ -1,9 +0,0 @@
-export interface GeocodeResult {
-    latitude: string;
-    longitude: string;
-    location: string;
-}
-type OsmCallback = (error: string | undefined, data: GeocodeResult | undefined) => void;
-export declare const geocode: (city: string, callback: OsmCallback) => void;
-export {};
-//# sourceMappingURL=geocode.d.ts.map
